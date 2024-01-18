f5c6686ea4b76cb5aaaf636acc72977fd8d53368
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2024-01-18 12:32:29 +0100

    Add files via upload

---
+++ Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Operators;
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+      int x=3;
+      int y=3;
+      y=y+2;
+      Console.WriteLine(y);
+      y +=2;
+      Console.WriteLine(y);
+       y /=1;
+       Console.WriteLine(y);
+       x *=2;
+       Console.WriteLine(x);
+
+       //Mantiksal Operatorler
+
+       bool isSucces = true;
+       bool isComplated = false;
+
+       if(isSucces && isComplated)
+       Console.WriteLine("perfect!");
+
+       if(isSucces || isComplated)
+        Console.WriteLine("Great!");
+
+         if(isSucces && isComplated)
+        Console.WriteLine("Fine!");
+
+    }
+}
